4f079a215f95637a793b3cd1e5d76a7952004c34
diablur <diablur@163.com>
2018-08-09 14:27:59 +0800

    modify build.gradle

--- src/main/java/com/boring/demo/service/MenuService.java
+++ src/main/java/com/boring/demo/service/MenuService.java
@@ -2,19 +2,16 @@ package com.boring.demo.service;
 
 import com.boring.demo.model.Menu;
 import com.boring.demo.repo.MenuRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+
+import javax.annotation.Resource;
 
 @Service
 public class MenuService {
 
-    private final MenuRepository menuRepository;
-
-    @Autowired
-    public MenuService(MenuRepository menuRepository) {
-        this.menuRepository = menuRepository;
-    }
+    @Resource
+    private MenuRepository menuRepository;
 
     public Flux<Menu> findAll() {
         return menuRepository.findAll();
